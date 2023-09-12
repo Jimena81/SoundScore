94b54e8c79190785a27d8802ac087983cfb8a05b
Jimena <jimenacastellanos81@gmail.com>
2023-09-12 20:24:59 +0200

    create users running

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers;
 use App\Http\Request\LoginRequest;
+//use App\Http\Request\CreateUserRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Auth;
 
 class AuthController extends Controller
 {
-    public function createUser(CreateUserRequest $request)
+    public function createUser(Request $request):JsonResponse
     {
         $user = User::create([
             'name'=> $request->name,
             'email'=> $request-> email,
             'password'=> Hash::make($request-> password)
         ]);
-        return respose ()->json([
+        return response()->json([
             'status'=> true,
             'message'=> 'User created successfully',
             'token'=> $user->createToken("API TOKEN")-> plainTextToken
         ], 200);
     }
 
-    public function loginUser(LoginRequest $request)
+    public function loginUser(LoginRequest $request):JsonResponse
     {
        if(!Auth::attempt($request->only(['email', 'password'])))
        {
